058c0232453b63b215c951a66e5a69678ad6e219
julien.sack <seigneur_s@hotmail.com>
2019-05-24 12:11:11 +0200

    version final TD

--- src/main/java/com/ipiecoles/java/java230/MyRunner.java
+++ src/main/java/com/ipiecoles/java/java230/MyRunner.java
@@ -148,10 +148,14 @@ public class MyRunner implements CommandLineRunner {
         }
 
 //controle le coefficient du commercial
-int performance = Integer.parseInt(commercialFields[6]);
-        if (performance < 0 || performance > 100){
+        try {
+            int performance = Integer.parseInt(commercialFields[6]);
+            if (performance < 0 || performance > 100) {
                 throw new BatchException("La performance du commercial est incorrecte : " + commercialFields[6]);
             }
+        } catch (Exception e) {
+            throw new BatchException("La performance du commercial est incorrecte : " + commercialFields[6]);
+        }
 // appel la fonction pour set les attributs communs
         setEmployes(c, commercialFields);
 //set les deux particuliers au commercial
@@ -174,11 +178,12 @@ int performance = Integer.parseInt(commercialFields[6]);
 
 // verifie et set le grade du technicien
         try {
-            if (Integer.parseInt(technicienFields[5]) <= 0 || Integer.parseInt(technicienFields[5]) > 5){
-                throw new BatchException("Le grade doit être compris entre 1 et 5 : " +technicienFields[5]);
-            }else{
-            t.setGrade(Integer.parseInt(technicienFields[5]));
-        }}catch (Exception e) {
+            if (Integer.parseInt(technicienFields[5]) <= 0 || Integer.parseInt(technicienFields[5]) > 5) {
+                throw new BatchException("Le grade doit être compris entre 1 et 5 : " + technicienFields[5]);
+            } else {
+                t.setGrade(Integer.parseInt(technicienFields[5]));
+            }
+        } catch (Exception e) {
             throw new BatchException("Le grade du technicien est incorrect " + technicienFields[5]);
         }
 //controle le matricule, le nombre de champs de la ligne, la date, et le salaire
@@ -190,14 +195,13 @@ int performance = Integer.parseInt(commercialFields[6]);
         }
 
 //set le matricule du manager si il est correct
-        employes.stream().forEach(emp-> {
+        employes.stream().forEach(emp -> {
             if (emp instanceof Manager && emp.getMatricule() == technicienFields[6]) {
                 t.setManager((Manager) emp);
             }
         });
 // verifie si le matricule existe en bdd
-        if (employeRepository.findByMatricule(technicienFields[6]) == null &&t.getManager() == null)
-        {
+        if (employeRepository.findByMatricule(technicienFields[6]) == null && t.getManager() == null) {
             throw new BatchException("Le manager de matricule " + technicienFields[6] + " n'a pas été trouvé dans le fichier ou en base de données");
         }
 
@@ -207,44 +211,48 @@ int performance = Integer.parseInt(commercialFields[6]);
         employes.add(t);
     }
 
-/** function commune pour verifier les champs communs des employes
-* @param  le matricule, le nombre de champs de la ligne, la date, et le salaire
-*/
-    private void checkChampsCommun(String[] fields, String RegexMatricule, Integer RegExnbChamps, String typeEmploye) throws BatchException{
+    /**
+     * function commune pour verifier les champs communs des employes
+     *
+     * @param le matricule, le nombre de champs de la ligne, la date, et le salaire
+     */
+    private void checkChampsCommun(String[] fields, String RegexMatricule, Integer RegExnbChamps, String typeEmploye) throws BatchException {
 
 //controle le matricule
-        if(!fields[0].matches(RegexMatricule)) {
+        if (!fields[0].matches(RegexMatricule)) {
             throw new BatchException("la chaîne : " + fields[0] + " ne respecte pas l'expression régulière : " + RegexMatricule);
         }
 
 //controle le nombre de champs
-        if(fields.length!=RegExnbChamps){
-            throw new BatchException(" La ligne  "+typeEmploye+ " ne contient pas "+RegExnbChamps+" éléments mais "+ fields.length);
+        if (fields.length != RegExnbChamps) {
+            throw new BatchException(" La ligne  " + typeEmploye + " ne contient pas " + RegExnbChamps + " éléments mais " + fields.length);
         }
 //controle la date
-        try{
+        try {
             DateTimeFormat.forPattern("dd/MM/yyyy").parseLocalDate(fields[3]);
-        }catch(Exception e){
-            throw new BatchException(fields[3]+" ne respecte pas le format de date dd/MM/yyyy");
+        } catch (Exception e) {
+            throw new BatchException(fields[3] + " ne respecte pas le format de date dd/MM/yyyy");
         }
 //controle le salaire
-        try{
+        try {
             Double.parseDouble(fields[4]);
-        }catch(Exception e){
-            throw new BatchException(fields[4]+" n'est pas un nombre valide pour un salaire");
+        } catch (Exception e) {
+            throw new BatchException(fields[4] + " n'est pas un nombre valide pour un salaire");
         }
     }
 
-/** fonction pour set les attributs communs des differents employés
-  * @param  le type d'employé, et le fields de l'employé
- */
-    private void setEmployes (Employe e, String[] fields){
+    /**
+     * fonction pour set les attributs communs des differents employés
+     *
+     * @param le type d'employé, et le fields de l'employé
+     */
+    private void setEmployes(Employe e, String[] fields) {
 
-    e.setMatricule(fields[0]);
-    e.setNom(fields[1]);
-    e.setPrenom(fields[2]);
-    e.setDateEmbauche(DateTimeFormat.forPattern("dd/MM/yyyy").parseLocalDate(fields[3]));
-    e.setSalaire(Double.parseDouble(fields[4]));
+        e.setMatricule(fields[0]);
+        e.setNom(fields[1]);
+        e.setPrenom(fields[2]);
+        e.setDateEmbauche(DateTimeFormat.forPattern("dd/MM/yyyy").parseLocalDate(fields[3]));
+        e.setSalaire(Double.parseDouble(fields[4]));
 
     }
 }
